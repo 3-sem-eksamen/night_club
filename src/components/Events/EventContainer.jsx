16b36b0a41eaafdcd18805cd95c8ab8a6e2a7f17
Margrethe1316 <maso0012@stud.kea.dk>
2026-05-05 18:37:03 +0200

    Add EventContainer component to render EventCard

---
+++ src/components/Events/EventContainer.jsx
@@ -0,0 +1,10 @@
+import EventCard from "./EventCard";
+const EventContainer = () => {
+    return ( 
+        <div>
+            <EventCard />
+        </div>
+     );
+}
+ 
+export default EventContainer;
