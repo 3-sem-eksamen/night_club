7ed6e83adf1e7d2595010106fc1d1a728a394781
Margrethe1316 <maso0012@stud.kea.dk>
2026-05-06 09:05:10 +0200

    Refactor EventCard and EventContainer components to accept dynamic event data and update layout styles

--- src/components/Events/EventCard.jsx
+++ src/components/Events/EventCard.jsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
 
-const EventCard = () => {
+const EventCard = ({ title, date, location, description, image, imgalt }) => {
     return (  
         <div className="grid grid-cols-2 gap-4">
-            <Image src="/assets/content-img/blog_full1.jpg" alt="Produkt billede" className="w-full h-full object-cover" width={500} height={500} />
+            <Image src={image} alt={imgalt} className="w-full h-full object-cover" width={500} height={500} />
             <div>
-            <h2>Neon Nights Grand Opening</h2>
-            <h3><span>May 9 | 21:00</span>CENTER STAGE</h3>
-            <p>Night Club opens its doors again with an evening of house, visuals, and live percussion on the main stage. Guests can expect welcome drinks from 21:00, an upgraded lighting setup, and a program that builds toward a big countdown show at midnight.</p>
+            <h2>{title}</h2>
+            <h3><span>{date}</span>{location}</h3>
+            <p>{description}</p>
             <button></button>
             </div>
         </div>
     );
 }
+
  
 export default EventCard;
--- src/components/Events/EventContainer.jsx
+++ src/components/Events/EventContainer.jsx
@@ -2,9 +2,23 @@ import EventCard from "./EventCard";
 const EventContainer = () => {
     return ( 
         <div>
-            <EventCard />
+            <FetchEvent />
         </div>
      );
 }
+
+const FetchEvent = async ()=>{
+    "use server"
+    const response = await fetch("http://localhost:4000/events")
+    const events = await response.json()
+
+    return events.map((event)=>{
+        return (
+        <div key={event.id}>
+        (<EventCard title={event.title} date={event.date} location={event.location} description={event.description} image={event.asset.url} imgalt={event.asset.alt}/>)
+        </div>
+        );
+    });
+};
  
 export default EventContainer;
