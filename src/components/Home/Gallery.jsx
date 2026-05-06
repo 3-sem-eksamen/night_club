7e7430f92af7f21c6c7e0a86638db0dd50c27a63
Emilie <emer0005@stud.kea.dk>
2026-05-06 09:38:48 +0200

    Refactor Gallery component to accept props for dynamic image rendering

--- src/components/Home/Gallery.jsx
+++ src/components/Home/Gallery.jsx
@@ -1,15 +1,7 @@
 import Image from "next/image";
 
-const GalleryImages = ["/assets/content-img/gallery1_big.jpg", "/assets/content-img/gallery2_big.jpg", "/assets/content-img/gallery3_big.jpg", "/assets/content-img/gallery4_big.jpg", "/assets/content-img/gallery5_big.jpg", "/assets/content-img/gallery6_big.jpg", "/assets/content-img/gallery7_big.jpg", "/assets/content-img/gallery8_big.jpg", "/assets/content-img/gallery9_big.jpg", "/assets/content-img/gallery10_big.jpg", "/assets/content-img/gallery11_big.jpg", "/assets/content-img/gallery12.jpg", "/assets/content-img/gallery13_big.jpg", "/assets/content-img/gallery14_big.jpg"];
-
-const Gallery = () => {
-  return (
-    <div>
-      {GalleryImages.map((image, i) => (
-        <Image key={i} src={image} alt="" width={500} height={500} />
-      ))}
-    </div>
-  );
+const Gallery = ({ img, width, height, imgalt }) => {
+  return <Image src={`http://localhost:4000${img}`} alt={imgalt} width={width} height={height} />;
 };
 
 export default Gallery;
