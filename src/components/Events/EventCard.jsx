4acfde6eb4f9e27340d5c71b8471e44d0a990439
Margrethe1316 <maso0012@stud.kea.dk>
2026-05-06 09:33:33 +0200

    Update EventCard and EventContainer components to use dynamic image source and improve configuration for local images

--- src/components/Events/EventCard.jsx
+++ src/components/Events/EventCard.jsx
@@ -1,9 +1,11 @@
+"use client";
+
 import Image from "next/image";
 
-const EventCard = ({ title, date, location, description, image, imgalt }) => {
+const EventCard = ({ title, date, location, description, img, imgalt }) => {
     return (  
         <div className="grid grid-cols-2 gap-4">
-            <Image src={image} alt={imgalt} className="w-full h-full object-cover" width={500} height={500} />
+            <Image src={`http://localhost:4000${img}`} alt={imgalt} className="w-full h-full object-cover" width={500} height={500} />
             <div>
             <h2>{title}</h2>
             <h3><span>{date}</span>{location}</h3>
