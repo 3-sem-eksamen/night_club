92a72e7a7b4222bbefcda100d769b270807460ed
Emilie <emer0005@stud.kea.dk>
2026-05-08 13:51:32 +0200

    Remove unused imports from Home component

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -1,7 +1,4 @@
 import Header from "@/components/GlobalComponents/Header";
-import Nav from "@/components/GlobalComponents/Navigation/Nav";
-import NewsContainer from "@/components/Home/NewsContainer";
-import VideoContainer from "@/components/Home/VideoContainer";
 
 export default function Home() {
   return <Header />;
