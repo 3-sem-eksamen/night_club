cbaae66d4c51f3e462928e94a9ae6e73d1473660
Emilie <emer0005@stud.kea.dk>
2026-05-08 09:53:36 +0200

    Add NewsContainer component for newsletter subscription

---
+++ src/components/Home/NewsContainer.jsx
@@ -0,0 +1,19 @@
+"use client"
+import Form from "next/form";
+import { useActionState, useactionState } from "react";
+
+const NewsContainer = () => {
+    const [state, emailAction, isPending] = useActionState(action, {message:""});
+    return ( 
+        <section>
+            <h3>Want the latest night club news</h3>
+            <p>Subscribe to our newsletter and never miss an <span>Event</span></p>
+            <Form action={emailAction}>
+                <input type="text" name="email" placeholder="Enter your email"/>
+                <button type="submit" {isPending ? "Submitting ..." : "Submit"}></button>
+            </Form>
+        </section>
+     );
+}
+ 
+export default NewsContainer;
