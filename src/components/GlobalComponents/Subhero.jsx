0a274acd29f7ab4b179874ed3a858e4bd2be6cd3
Margrethe1316 <maso0012@stud.kea.dk>
2026-05-08 11:15:18 +0200

    Add Subhero component for enhanced section display

---
+++ src/components/GlobalComponents/Subhero.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+const Subhero = ({title}) => {
+    return ( 
+        <section>
+            <Image src="assets/bg/footerbg.jpg"
+            alt="footer background"
+            width={1620}
+            height={500}
+            />
+            <h2>{title}</h2>
+            <Image src="assets/bottom_line.png"
+            alt="bottom line"
+            width={1363}
+            height={109}
+            />
+
+        </section>
+     );
+}
+ 
+export default Subhero;
