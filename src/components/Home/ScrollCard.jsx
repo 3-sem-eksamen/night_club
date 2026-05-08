bc18711a95f06da8ef839958a10f960b3be3e348
Margrethe1316 <maso0012@stud.kea.dk>
2026-05-08 09:09:13 +0200

    Add ScrollCard component to display images in a scrollable section

---
+++ src/components/Home/ScrollCard.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const ScrollCard = () => {
+    return ( 
+        <section>
+            <Image>
+                src={img}
+                width={233}
+                height={217}
+            </Image>
+        </section>
+     );
+}
+ 
+export default ScrollCard;
