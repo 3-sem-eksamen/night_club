744405722fb683d56bde3c1dcf95d7d6972fc82e
Emilie <emer0005@stud.kea.dk>
2026-05-08 09:53:43 +0200

    Fix rendering of success message in NewsContainer component

--- src/components/Home/NewsContainer.jsx
+++ src/components/Home/NewsContainer.jsx
@@ -10,8 +10,12 @@ const NewsContainer = () => {
             <p>Subscribe to our newsletter and never miss an <span>Event</span></p>
             <Form action={emailAction}>
                 <input type="text" name="email" placeholder="Enter your email"/>
+                {state?.message && (
+                    <p>${state.succes}</p>
+                )}
                 <button type="submit" {isPending ? "Submitting ..." : "Submit"}></button>
             </Form>
+
         </section>
      );
 }
