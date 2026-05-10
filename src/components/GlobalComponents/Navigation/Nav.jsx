72c7f14632602b8f6085ad169dead80868fb77ab
Margrethe1316 <maso0012@stud.kea.dk>
2026-05-10 09:27:50 +0200

    Fix formatting in Nav component for improved readability

--- src/components/GlobalComponents/Navigation/Nav.jsx
+++ src/components/GlobalComponents/Navigation/Nav.jsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 const Nav = () => {
   return (
     <ul className="flex gap-8">
-  <li>
-    <Link href="/">Home</Link>
-  </li>
-  <li>
-    <Link href="/Events">Events</Link>
-  </li>
-  <li>
-    <Link href="/BookTable">Book table</Link>
-  </li>
-  <li>
-    <Link href="/ContactUs">Contact us</Link>
-  </li>
-</ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/Events">Events</Link>
+      </li>
+      <li>
+        <Link href="/BookTable">Book table</Link>
+      </li>
+      <li>
+        <Link href="/ContactUs">Contact us</Link>
+      </li>
+  </ul> 
   );
 };
 
