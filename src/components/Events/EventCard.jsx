f71def1b5e4ea0101507d4f3204563f95bc1bf8b
Margrethe1316 <maso0012@stud.kea.dk>
2026-05-10 12:52:44 +0200

    Refactor EventCard component layout and styling for improved readability and presentation

--- src/components/Events/EventCard.jsx
+++ src/components/Events/EventCard.jsx
@@ -6,9 +6,9 @@ const EventCard = ({ title, date, location, description, img, imgalt }) => {
     return (  
         <div className="">
             <Image src={`http://localhost:4000${img}`} alt={imgalt} className="w-full h-full object-cover" width={500} height={500} />
-            <div className="max-w-md mx-auto">
-            <h2 className="text-[var(--step-4)]">{title}</h2>
-            <h3><span>{date}</span>{location}</h3>
+            <div className="max-w-md mx-auto flex flex-col gap-[var(--space-s)]">
+            <h2 className="">{title}</h2>
+            <h3><span className="text-[var(--color-surface-highlight-primary)]">{date}</span>{location}</h3>
             <p>{description}</p>
             <button></button>
             </div>
