8b0a3df11a3a63564db4035377e54a76ddf0b578
Margrethe1316 <maso0012@stud.kea.dk>
2026-05-10 13:16:53 +0200

    Add ContactUs page with Header, Subhero, and Contact components

---
+++ src/app/(pages)/ContactUs/page.js
@@ -0,0 +1,13 @@
+import Header from "@/components/GlobalComponents/Header";
+import Subhero from "@/components/GlobalComponents/Subhero";
+import Contact from "@/components/ContactUs/Contact";
+
+export default function Home() {
+  return (
+    <section>
+      <Header />
+      <Subhero />
+      <Contact />
+    </section>
+  );
+}
