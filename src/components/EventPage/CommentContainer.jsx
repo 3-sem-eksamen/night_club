919ba0dfe321e866196c993b0f51f50832896afb
Margrethe1316 <maso0012@stud.kea.dk>
2026-05-13 10:10:59 +0200

    Refactor CommentContainer to use react-hook-form for form handling and validation

--- src/components/EventPage/CommentContainer.jsx
+++ src/components/EventPage/CommentContainer.jsx
@@ -1,19 +1,57 @@
 "use client";
-import Form from "next/form";
-import { useActionState } from "react";
-import commentaction from "@/app/action/commentaction";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useParams} from "next/navigation";
+
+const commentSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string("Email is required").email("Invalid email address"),
+  content: z.string().min(1, "Comment content is required"),
+});
 
 const CommentContainer = () => {
-  const [commentstate, commentAction, isPending] = useActionState(commentaction, { message: "" });
+  const params = useParams();
+  console.log(params);
+  const {
+    register,
+    handleSubmit,
+    formState: {errors, isSubmitting}, 
+    reset,  
+  } = useForm({
+    resolver:zodResolver(commentSchema),
+    mode: "onChange",
+  });
+
+  const onSubmit = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const date = new Date().toISOString();
+    console.log(data)
+    await fetch("http://localhost:4000/comments", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        eventId: 1,
+        name: data.name ,
+        content: data.content ,
+        date: date,
+      }),
+    });
+
+    reset();
+  };
   return (
     <section>
-      <Form action={commentAction}>
-        <input type="text" name="name" placeholder="Your name" />
-        <input type="text" name="email" placeholder="Your email" />
-        <textarea name="content" rows={4} placeholder="Your comment" />
-        {commentstate?.message && <span>{commentstate.message}</span>}
-        <button type="submit">{isPending ? "Submitting.." : "Submit"}</button>
-      </Form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("name")} type="text" name="name" placeholder="Your name" />
+        {errors.name && <p>{errors.name.message}</p>}
+        <input {...register("email")} type="text" name="email" placeholder="Your email" />
+        {errors.email && <p>{errors.email.message}</p>}
+        <textarea {...register("content")} name="content" rows={4} placeholder="Your comment" />
+        {errors.content && <p>{errors.content.message}</p>}
+        <button disabled={isSubmitting} type="submit">{isSubmitting ? "Sender..." : "Send"}</button>
+        
+      </form>
     </section>
   );
 };
