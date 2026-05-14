458031eaff18744823b6dde91101beb9da281cd3
Margrethe1316 <maso0012@stud.kea.dk>
2026-05-14 12:20:04 +0200

    Refactor BookTable component to implement form validation and handle table bookings

--- src/app/(pages)/BookTable/page.js
+++ src/app/(pages)/BookTable/page.js
@@ -3,13 +3,13 @@ import Header from "@/components/GlobalComponents/Header";
 import Subhero from "@/components/GlobalComponents/Subhero";
 import BookTable from "@/components/BookTable/BookTable";
 
-export default function Home() {
+export default function BookTablePage({ searchParams }) {
   return (
     <section>
       <Header />
       <Subhero />
       <TablesContainer />
-      <BookTable />
+      <BookTable id={searchParams.eventId} />
     </section>
   );
 }
