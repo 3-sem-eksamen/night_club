317cf7cd598ed04cf18b1ad215af4db694f7fff4
Margrethe1316 <maso0012@stud.kea.dk>
2026-05-15 10:42:19 +0200

    Refactor PlayingCard and TrackContainer components to implement track navigation and media handling

--- src/components/Home/PlayingCard.jsx
+++ src/components/Home/PlayingCard.jsx
@@ -1,11 +1,33 @@
 "use client";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
+import { useState } from "react";
+import Image from "next/image";
+
+
+const media = [
+  {image: "/assets/content-img/track_thumb.jpg", track:"/assets/media/black-box-funky.mp3" }
+  {image: "/assets/content-img/track1.jpg", track:"/assets/media/euphoria.mp3" }
+  {image: "/assets/content-img/track2.jpg", track:"/assets/media/fashion-red-tape.mp3" }
+]
+
 
 const PlayingCard = () => {
+const [currentTrack, setCurrentTrack] = useState(0); 
+
+  const handleNext = () => {
+    console.log(handleNext)
+    setCurrentTrack(prev => prev + 1 < tracks.length ? prev + 1 : 0)
+  }
+
+  const handlePrev = () => {
+     setCurrentTrack(prev => prev - 1 >= 0 ? prev - 1 : tracks.length - 1)
+  }
+
   return (
     <>
-      <AudioPlayer className="bg-red-200" autoPlay src="/assets/media/black-box-funky.mp3" onPlay={(e) => console.log("OnPlay")} />
+<Image src=""></>
+      <AudioPlayer className="bg-red-200" showJumpControls={false} onClickNext={handleNext} onClickPrevious={handlePrev} showSkipControls src={tracks [currentTrack]} onPlay={(e) => console.log("OnPlay")} />
     </>
   );
 };
--- src/components/Home/TrackContainer.jsx
+++ src/components/Home/TrackContainer.jsx
@@ -1,9 +1,42 @@
 import PlayingCard from "./PlayingCard";
+import {useStae} from ".react";
+import ScrollContainer from "./ScrollContainer";
+
+const media = [
+  {image: "/assets/content-img/track_thumb.jpg", track:"/assets/media/black-box-funky.mp3" }
+  {image: "/assets/content-img/track1.jpg", track:"/assets/media/euphoria.mp3" }
+  {image: "/assets/content-img/track2.jpg", track:"/assets/media/fashion-red-tape.mp3" }
+]
 
 const TrackContainer = () => {
+const [currentMedia, setCurrentMedia] = useState(0); 
+
+  const handleNext = () => {
+    console.log(handleNext)
+    setCurrentMedia(prev => prev + 1 < media.length ? prev + 1 : 0)
+  }
+
+  const handlePrev = () => {
+     setCurrentMedia(prev => prev - 1 >= 0 ? prev - 1 : media.length - 1)
+  }
+
+  const selectedMedia =(mediaIndex)=>{
+    setCurrentMedia(mediaIndex);
+  }
+
+
   return (
     <section>
-      <PlayingCard />
+      <PlayingCard
+      item={media[currentIndex]}
+      handleNext={handleNext}
+      handlePrev={handlePrev}
+       />
+      <ScrollContainer
+      media={media}
+      currentMedia={currentMedia}
+      selectedMedia={selectedMedia}
+      />
     </section>
   );
 };
