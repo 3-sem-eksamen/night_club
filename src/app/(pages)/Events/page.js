86cc669129552ee9aad2b5091b47b113b896ccfc
Emilie <emer0005@stud.kea.dk>
2026-05-19 10:08:06 +0200

    Refactor EventCard and Header components for improved structure and readability; update Events page to use EventCard

--- src/app/(pages)/Events/page.js
+++ src/app/(pages)/Events/page.js
@@ -1,13 +1,14 @@
+import EventCard from "@/components/Events/EventCard";
 import EventContainer from "@/components/Events/EventContainer";
 import Header from "@/components/GlobalComponents/Header";
 import Subhero from "@/components/GlobalComponents/Subhero";
 
-export default function Home() {
+export default function Events() {
   return (
     <section>
       <Header />
       <Subhero />
-      <EventContainer />
+      <EventCard />
     </section>
   );
 }
--- src/components/Events/EventCard.jsx
+++ src/components/Events/EventCard.jsx
@@ -1,26 +1,27 @@
 "use client";
-
 import Image from "next/image";
-import { eventDate} from "../../app/dateConverter.js";
+import { eventDate } from "../../app/dateConverter.js";
 import Link from "next/link.js";
 import PrimaryBtn from "../GlobalComponents/PrimaryBtn.jsx";
 
 const EventCard = ({ title, date, location, description, img, imgalt, slug, id }) => {
-    console.log({img})
-    return (  
-        <div className="">
-            <Image src={`${process.env.api_url}${img}`} alt={imgalt} className="w-full h-full object-cover" width={500} height={500} />
-            <div className="max-w-md mx-auto flex flex-col gap-[var(--space-s)]">
-            <h2 className="">{title}</h2>
-            <h3><span className="text-[var(--color-surface-highlight-primary)]">{eventDate(date)}</span>{location}</h3>
-            <p>{description}</p>
-            <Link className="mx-auto" href={`/EventPage/${slug}/${id}`}>
-            <PrimaryBtn text="Read more"/>
-            </Link>
-            </div>
-        </div>
-    );
-}
+  console.log("heeeeej", img);
+  return (
+    <div className="">
+      <Image src={`${process.env.api_url}${img}`} alt={imgalt} className="w-full h-full object-cover" width={500} height={500} />
+      <div className="max-w-md mx-auto flex flex-col gap-[var(--space-s)]">
+        <h2 className="">{title}</h2>
+        <h3>
+          <span className="text-[var(--color-surface-highlight-primary)]">{eventDate(date)}</span>
+          {location}
+        </h3>
+        <p>{description}</p>
+        <Link className="mx-auto" href={`/EventPage/${slug}/${id}`}>
+          <PrimaryBtn text="Read more" />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
- 
 export default EventCard;
