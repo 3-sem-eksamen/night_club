71dff08e35fe1c4c4358fe1850af645a739fc7ef
Emilie <emer0005@stud.kea.dk>
2026-05-19 16:31:24 +0200

    Enhance BookTableForm layout with improved styling; add error handling for form inputs

--- src/components/BookTable/BookTableForm.jsx
+++ src/components/BookTable/BookTableForm.jsx
@@ -75,49 +75,63 @@ const BookTable = ({ events, setAvailableTables, filteredAvailableTables, setTak
   };
   return (
     <section>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h2>Booking for:</h2>
-        <input {...register("name")} type="text" name="name" placeholder="Your name" />
-        {errors.name && <p>{errors.name.message}</p>}
-        <input {...register("email")} type="text" name="email" placeholder="Your email" />
-        {errors.email && <p>{errors.email.message}</p>}
-        <select {...register("eventNight")} name="eventNight" onChange={handleEventChange}>
-          <option value="">Select event night</option>
+      <h2>Book a table</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className="grid gap-6 grid-cols-[repeat(auto-fit,minmax(500px,1fr))] w-full justify-center">
+        <div>
+          <input {...register("name")} type="text" name="name" placeholder="Your name" />
+          {errors.name && <p>{errors.name.message}</p>}
+        </div>
+        <div>
+          <input {...register("email")} type="text" name="email" placeholder="Your email" />
+          {errors.email && <p>{errors.email.message}</p>}
+        </div>
+        <div>
+          <select {...register("eventNight")} name="eventNight" onChange={handleEventChange}>
+            <option value="">Select event night</option>
 
-          {events.map((event) => (
-            <option key={event.id} value={event.id}>
-              {event.title}
-              {event.date}
-            </option>
-          ))}
-        </select>
-        {errors.eventNight && <p>{errors.eventNight.message} </p>}
-        <select {...register("guests")} name="guests" onChange={handleGuestChange}>
-          <option value="">Number of guests</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-        </select>
-        {errors.guests && <p>{errors.guests.message}</p>}
-        <select {...register("table")} name="table">
-          <option value="">Select table</option>
-          {filteredAvailableTables.map((table) => (
-            <option key={table} value={table}>
-              Bord {table}
-            </option>
-          ))}
-        </select>
-        {filteredAvailableTables.length === 0 && <p>Der er ingen borde der passer til dit valg</p>}
-        {errors.table && <p>{errors.table.message}</p>}
-        <input {...register("phone")} type="text" name="phone" placeholder="Your phone number" />
-        {errors.phone && <p>{errors.phone.message}</p>}
-        <input {...register("content")} type="text" name="content" placeholder="Your comment" />
-        {errors.content && <p>{errors.content.message}</p>}
+            {events.map((event) => (
+              <option key={event.id} value={event.id}>
+                {event.title}
+                {event.date}
+              </option>
+            ))}
+          </select>
+          {errors.eventNight && <p>{errors.eventNight.message} </p>}
+        </div>
+        <div>
+          <select {...register("guests")} name="guests" onChange={handleGuestChange}>
+            <option value="">Number of guests</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+          </select>
+          {errors.guests && <p>{errors.guests.message}</p>}
+        </div>
+        <div>
+          <select {...register("table")} name="table">
+            <option value="">Select table</option>
+            {filteredAvailableTables.map((table) => (
+              <option key={table} value={table}>
+                Bord {table}
+              </option>
+            ))}
+          </select>
+          {filteredAvailableTables.length === 0 && <p>Der er ingen borde der passer til dit valg</p>}
+          {errors.table && <p>{errors.table.message}</p>}
+        </div>
+        <div>
+          <input {...register("phone")} type="text" name="phone" placeholder="Your phone number" />
+          {errors.phone && <p>{errors.phone.message}</p>}
+        </div>
+        <div className="col-span-full">
+          <textarea {...register("content")} type="text" name="content " rows={5} placeholder="Your comment" />
+          {errors.content && <p>{errors.content.message}</p>}
+        </div>
         <button disabled={isSubmitting} type="submit">
           {isSubmitting ? "Sender..." : "Send"}
         </button>{" "}
