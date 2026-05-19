d832e99c06618d6d6c1f32fff868206c05672d61
Emilie <emer0005@stud.kea.dk>
2026-05-19 15:51:32 +0200

    Enhance BookTable and Filter components with improved styling and layout; add title prop to Subhero for better context

--- src/components/BookTable/Filter.jsx
+++ src/components/BookTable/Filter.jsx
@@ -1,11 +1,19 @@
 "use client";
 const Filter = ({ setNumberOfGuest }) => {
   return (
-    <section>
-      <button onClick={() => setNumberOfGuest("")}>All</button>
-      <button onClick={() => setNumberOfGuest("4")}>Table for 4 people</button>
-      <button onClick={() => setNumberOfGuest("6")}>Table for 6 people</button>
-      <button onClick={() => setNumberOfGuest("8")}>Table for 8 people</button>
+    <section className="flex gap-5">
+      <button className="filter_knap" onClick={() => setNumberOfGuest("")}>
+        All tables
+      </button>
+      <button className="filter_knap" onClick={() => setNumberOfGuest("4")}>
+        Table for 4 people
+      </button>
+      <button className="filter_knap" onClick={() => setNumberOfGuest("6")}>
+        Table for 6 people
+      </button>
+      <button className="filter_knap" onClick={() => setNumberOfGuest("8")}>
+        Table for 8 people
+      </button>
     </section>
   );
 };
--- src/components/GlobalComponents/Subhero.jsx
+++ src/components/GlobalComponents/Subhero.jsx
@@ -1,7 +1,8 @@
+"use client";
 import Image from "next/image";
 const Subhero = ({ title }) => {
   return (
-    <section className="grid">
+    <section className="grid grid-cols-1 grid-rows-1 ">
       <Image className="col-start-1 row-start-1 w-full h-full object-cover opacity-50" src="/assets/bg/footerbg.jpg" alt="footer background" width={1620} height={500} />
       <div className="col-start-1 row-start-1 flex flex-col items-center justify-center z-10">
         <h2>{title}</h2>
